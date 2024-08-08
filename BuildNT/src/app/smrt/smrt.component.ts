595ee07f2325b01e016eea52a2b4dd1b33b0c9fa
[ajay-r-dev] <[ajayrcloudengineer@gmail.com]>
2024-08-08 20:47:43 +0530

    client-list

---
+++ BuildNT/src/app/smrt/smrt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smrt',
+  standalone: true,
+  imports: [],
+  templateUrl: './smrt.component.html',
+  styleUrl: './smrt.component.css'
+})
+export class SmrtComponent {
+
+}
