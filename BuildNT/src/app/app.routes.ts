b0cfc74f431f92b8a310fe4bdbe7902cac6a598b
maha424 <mahalakshmi87780@gmail.com>
2024-08-09 14:58:19 +0530

    myproject

--- BuildNT/src/app/app.routes.ts
+++ BuildNT/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppAlignmentComponent } from './app-alignment/app-alignment.component';
 import { ClientListComponent } from './client-list/client-list.component';
+import { MyProjectComponent } from './my-project/my-project.component';
 
 export const routes: Routes = [
     {
@@ -21,6 +22,10 @@ export const routes: Routes = [
             {
                 path: 'clientList',
                 component:ClientListComponent,
+            },
+            {
+                path: 'myproject',
+                component:MyProjectComponent,
             },
         ]
     },
