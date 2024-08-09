84b3c59a76d647e5adbaf93248476964fbd278ce
NivethaSelvam27 <nivethaselvam.tvmmca@gmail.com>
2024-08-09 19:55:05 +0530

    header changes

--- BuildNT/src/app/app.routes.ts
+++ BuildNT/src/app/app.routes.ts
@@ -30,6 +30,7 @@ export const routes: Routes = [
                 path: 'myproject',
                 component:MyProjectComponent,
             },
+            {
                 path:'employ',
                 component:EmployComponent,
             },
